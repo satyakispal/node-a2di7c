43c31045d0efb557db1346e0c5abcc228ae18e9b
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-03 19:07:56 +0530

    1 file modified

--- new1.js
+++ new1.js
@@ -3,3 +3,9 @@
 // let a = 074;
 // console.log(a);
 // // SyntaxError: Octal literals are not allowed in strict mode
+
+// let personal = {firstname: 'Satyaki'};
+// delete personal;
+// delete personal.firstname;
+// console.log(personal);
+// // SyntaxError: Delete of an unqualified identifier in strict mode
