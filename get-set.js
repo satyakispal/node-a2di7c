6c34043edc276b72de6ac25ea32a4690860f81d7
satyakispal <99709847+satyakispal@users.noreply.github.com>
2022-11-03 18:51:31 +0530

    1 file modified

--- get-set.js
+++ get-set.js
@@ -15,3 +15,31 @@ arc.weight; // 'get!'
 
 arc.weight = 30;
 // TypeError: setting getter-only property "weight"
+
+function Archiver() {
+  let weight = null;
+  const archive = [];
+
+  Object.defineProperty(this, 'weight', {
+    get() {
+      console.log('get!');
+      return weight;
+    },
+    set(value) {
+      weight = value;
+      archive.push({ val: weight });
+    },
+  });
+
+  this.getArchive = function () {
+    return archive;
+  };
+}
+
+const arc = new Archiver();
+arc.weight; // 'inside get!' (output)
+arc.weight = 2;
+arc.weight = 11;
+arc.weight = 16;
+
+arc.getArchive(); // [{ val: 2 }, { val: 11 }, { val: 16 }]
